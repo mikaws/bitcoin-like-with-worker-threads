a1a2db4bce3b2057576862a119d11dd62f3483a6
MichaelHenrique <michaelhenriqued@gmail.com>
2021-06-10 04:02:58 -0300

    Parent process

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const { payloads } = require('./payloads.json')
-const final = []
-let finisheWorkers = 0
+const { Worker, isMainThread } = require('worker_threads')
+
 const LEADING_ZEROES = 4
+const final = []
+let finishedWorkers = 0
+
+if(isMainThread){
+  for(let payload of payloads){
+    const worker = new Worker(__filename, {env: { LEADING_ZEROES } })
+      worker.once('message', (message) => {
+        final.push(message)
+        finishedWorkers++
+        if(finishedWorkers === payloads.length) console.log(final)
+      })
+      worker.on('error', console.error)
+
+      console.log(`Iniciando worker de ID ${worker.threadId} e enviando o payload "${payload}"`)
+    }
+  }
+
+
