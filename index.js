e8d929a10e75e328b74a9465ff40584def8951eb
MichaelHenrique <michaelhenriqued@gmail.com>
2021-06-10 04:30:34 -0300

    Splitting into two files

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
 const { payloads } = require('./payloads.json')
-const { crypto } = require('crypto')
-const { Worker, isMainThread, parentPort } = require('worker_threads')
+const { Worker } = require('worker_threads')
 
 const LEADING_ZEROES = 4
 const final = []
 let finishedWorkers = 0
 
-if(isMainThread){
-  for(let payload of payloads){
-    const worker = new Worker(__filename, {env: { LEADING_ZEROES } })
-      worker.once('message', (message) => {
-        final.push(message)
-        finishedWorkers++
-        if(finishedWorkers === payloads.length) console.log(final)
-      })
-      worker.on('error', console.error)
+for(let payload of payloads){
+  const worker = new Worker('./worker.js', {env: { LEADING_ZEROES } })
+    worker.once('message', (message) => {
+      final.push(message)
+      finishedWorkers++
+      if(finishedWorkers === payloads.length) console.log(final)
+    })
+    worker.on('error', console.error)
 
-      console.log(`Iniciando worker de ID ${worker.threadId} e enviando o payload "${payload}"`)
-  }
-} else {
-  parentPort.once('message', (message) => {
-    const payload = message
-    let nonce = 0
-    let generatedHash = ''
-
-    do {
-      generatedHash = crypto.createHash('sha256').update(payload + nonce).digest('hex')
-      nonce++
-    } while (generatedHash.slice(0, process.env.LEADING_ZEROES) !== '0'.repeat(process.env.LEADING_ZEROES))
-
-    parentPort.postMessage({ payload: message, nonce, hash: generatedHash})
-  })
+    console.log(`Iniciando worker de ID ${worker.threadId} e enviando o payload "${payload}"`)
+    worker.postMessage(payload)
 }
